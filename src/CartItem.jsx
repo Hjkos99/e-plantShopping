eedc7db113c13110a033717ce188b7687061153b
Hjkos99 <hegedus.akos99@gmail.com>
2024-11-22 08:21:36 -0500

    Task3/1task completed

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,16 +9,22 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    totalAmount = item.quantity * item.cost
+    return (totalAmount)
   };
 
   const handleContinueShopping = (e) => {
-   
+
   };
+
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
 
 
   const handleIncrement = (item) => {
+
   };
 
   const handleDecrement = (item) => {
@@ -57,7 +63,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
